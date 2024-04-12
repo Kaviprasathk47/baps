8dae7a8edc72c2f0f6a43b77d03fa1c5342c4cc9
Kaviprasathk47 <kaiprasath047@gmail.com>
2024-04-12 14:05:02 +0530

    hi nk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Login from './login'; // Assuming the login component is exported as default from 'login.js' or 'login.jsx'
+import Login from './login';
+
+
 
 function App() {
   return (
     <div>
       <Login />
+      
+      
     </div>
   );
 }
--- src/login.jsx
+++ src/login.jsx
@@ -1,43 +1,72 @@
 import React, { useState } from 'react';
-import './login.css'; 
-import { FiUser } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { FaLock } from "react-icons/fa6";
+import './login.css';
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState(''); 
+  const [password, setPassword] = useState('');
+  const [visible, setVisible] = useState(false);
 
-  const handleSubmit = (event) => { 
+  const toggleVisibility = () => {
+    setVisible(prevVisible => !prevVisible);
+  };
+
+  const Icon = visible ? <FaRegEyeSlash onClick={toggleVisibility} /> : <FaRegEye onClick={toggleVisibility} />;
+  const InputType = visible ? "text" : "password";
+
+  const handleSubmit = (event) => {
     event.preventDefault();
     console.log('Login attempted Name:', username);
-    console.log('Login attempted Password:',password); 
+    console.log('Login attempted Password:', password);
+  
   };
 
   return (
-    <div className="login-form">
+   
+    <div className="loginform">
       <form onSubmit={handleSubmit}>
+      <FaUserCircle  className="userIcon" />
         <input
+        className='user'
           type="text"
-          placeholder="Username"  
+          placeholder="    User ID"
           value={username}
           onChange={(event) => setUsername(event.target.value)}
           required
+          maxLength={10}
+          
         />
-        <FiUser />
-        <br /> 
-
-        <input
-          type="password" 
-          placeholder="Password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          required
-        />
-        <FaLock />
+        
+       
         <br />
+        <FaLock className="lockIcon"  />
 
+        <div>
+         
+          <input
+           className='lock'
+            type={InputType}
+            placeholder="    Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          {Icon}
+        </div>
+        <br />
         <button type="submit">Login</button>
+        <p>_______________or________________</p>
+        <div className='button-container'>
+  <FcGoogle className='google'/>
+  <button type='submit' className='butcolor'>  Sign-In with GOOGLE</button>
+</div>
+
+        
       </form>
+      
     </div>
   );
 };
